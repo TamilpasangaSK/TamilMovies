22ad98d6135b1572e8c93df0c38401ea9dc2484f
TamilpasangaSK <iamsanthoshmca2022@gmail.com>
2025-09-30 19:37:51 +0530

    Fix movie quality rendering in MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -40,7 +40,7 @@ const MovieCard = ({ movie }) => {
             className="w-full h-64 object-cover"
           />
           <div className="absolute top-2 right-2 bg-black bg-opacity-70 text-white px-2 py-1 rounded text-sm">
-            {movie.quality}
+            {movie.quality?.format || 'HD'}
           </div>
           <div className="absolute bottom-2 left-2 bg-black bg-opacity-70 text-white px-2 py-1 rounded text-sm">
             {formatViews(movie.views)} views
