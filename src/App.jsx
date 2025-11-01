153ef4df52c9aa0bf3330516f5a837fff88b4788
Santhoshkumar.S <iamsanthoshmca2022@gmail.com>
2025-11-01 15:04:39 +0530

    Fix missing Link import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Header from './components/Header';
 import AdBlockDetector from './components/AdBlockDetector';
